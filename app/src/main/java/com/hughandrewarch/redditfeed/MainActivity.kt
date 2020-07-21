280bdca15a6f74aca99fe3490a516c63a12256fb
hughandrewarch <hughandrewarch@gmail.com>
2020-07-21 09:41:04 -0400

    streamline view model

--- app/src/main/java/com/hughandrewarch/redditfeed/MainActivity.kt
+++ app/src/main/java/com/hughandrewarch/redditfeed/MainActivity.kt
@@ -1,22 +1,34 @@
 package com.hughandrewarch.redditfeed
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import com.hughandrewarch.redditfeed.databinding.ActivityMainBinding
+import com.hughandrewarch.redditfeed.domain.model.Subreddit
 import com.hughandrewarch.redditfeed.viewmodels.SubredditViewModel
 
 class MainActivity : AppCompatActivity() {
 
+    private val subredditViewModel: SubredditViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        subredditViewModel.subreddit.observe(this, subredditObserver)
 
         val activityMainBinding: ActivityMainBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        activityMainBinding.viewModel = SubredditViewModel()
+        activityMainBinding.viewModel = subredditViewModel
         activityMainBinding.executePendingBindings()
+    }
+
+    val subredditObserver = Observer<Subreddit> { subreddit ->
+        // Update the UI, in this case, a TextView.
+        Toast.makeText(this, "HELLO", Toast.LENGTH_SHORT).show()
     }
 
 }
--- app/src/main/java/com/hughandrewarch/redditfeed/viewmodels/SubredditViewModel.kt
+++ app/src/main/java/com/hughandrewarch/redditfeed/viewmodels/SubredditViewModel.kt
@@ -1,19 +1,26 @@
 package com.hughandrewarch.redditfeed.viewmodels
 
 import androidx.databinding.BaseObservable
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.hughandrewarch.redditfeed.domain.model.Subreddit
 import com.hughandrewarch.redditfeed.network.adapters.NetworkRedditRepo
 import io.reactivex.rxjava3.kotlin.subscribeBy
 
-class SubredditViewModel: BaseObservable() {
-    private lateinit var subreddit: Subreddit
+class SubredditViewModel: ViewModel() {
+//    private lateinit var subreddit: Subreddit
+
+    val subreddit: MutableLiveData<Subreddit> by lazy {
+        MutableLiveData<Subreddit>()
+    }
 
     fun onLoginClicked()  {
         val network = NetworkRedditRepo()
         network.getSubreddit("kotlin")
             .subscribeBy(
                 onNext =  {
-                    subreddit = it
+                    subreddit.value = it
                 },
                 onError = { println(it) }
             )
