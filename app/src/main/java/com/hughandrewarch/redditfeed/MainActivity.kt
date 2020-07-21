d6c8d78a66fa124eb797de2d8ad4f170cd2966f3
hughandrewarch <hughandrewarch@gmail.com>
2020-07-21 18:10:00 -0400

    attach subreddit viewmodel to the main activity

--- app/src/main/java/com/hughandrewarch/redditfeed/MainActivity.kt
+++ app/src/main/java/com/hughandrewarch/redditfeed/MainActivity.kt
@@ -3,21 +3,61 @@ package com.hughandrewarch.redditfeed
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import com.hughandrewarch.redditfeed.databinding.ActivityMainBinding
+import com.hughandrewarch.redditfeed.domain.model.Subreddit
 import com.hughandrewarch.redditfeed.viewmodels.SubredditViewModel
-import com.hughandrewarch.redditfeed.views.PostListFragment
-import com.hughandrewarch.redditfeed.views.adapter.PostViewAdapter
+import com.hughandrewarch.redditfeed.views.fragments.PostListFragment
 
 class MainActivity : AppCompatActivity() {
 
     private val subredditViewModel: SubredditViewModel by viewModels()
-    private lateinit var postViewAdapter: PostViewAdapter
+    private val postListFragment = PostListFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val activityMainBinding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         val fragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frame, PostListFragment())
+        setFragmentArgument("onCreate")
+
+        fragmentTransaction.replace(R.id.frame,
+            postListFragment
+        )
         fragmentTransaction.commit()
+
+        subredditViewModel.subreddit.observe(this, subredditObserver)
+        activityMainBinding.viewModel = subredditViewModel
+        activityMainBinding.executePendingBindings()
+    }
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//
+//        val activityMainBinding: ActivityMainBinding =
+//            DataBindingUtil.setContentView(this, R.layout.activity_main)
+//
+//
+//        postViewAdapter = PostViewAdapter(this, arrayListOf())
+//
+//        val postRecyclerView: RecyclerView = findViewById(R.id.post_list)
+//        postRecyclerView.layoutManager = LinearLayoutManager(this)
+//        postRecyclerView.adapter = postViewAdapter
+//    }
+
+    private val subredditObserver = Observer<Subreddit> { subreddit ->
+        postListFragment.setPosts(subreddit.children)
+    }
+
+    private fun setFragmentArgument(test: String) {
+
+        val bundle = Bundle()
+        bundle.putString("test", test)
+        postListFragment.arguments = bundle
+
     }
 }
--- app/src/main/java/com/hughandrewarch/redditfeed/views/fragments/PostListFragment.kt
+++ app/src/main/java/com/hughandrewarch/redditfeed/views/fragments/PostListFragment.kt
@@ -1,21 +1,17 @@
-package com.hughandrewarch.redditfeed.views
+package com.hughandrewarch.redditfeed.views.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.hughandrewarch.redditfeed.R
-import com.hughandrewarch.redditfeed.domain.model.Subreddit
-import com.hughandrewarch.redditfeed.viewmodels.SubredditViewModel
+import com.hughandrewarch.redditfeed.domain.model.Post
 import com.hughandrewarch.redditfeed.views.adapter.PostViewAdapter
 import kotlinx.android.synthetic.main.post_list_fragment.*
 
 class PostListFragment: Fragment() {
-    private val subredditViewModel: SubredditViewModel by activityViewModels()
     private lateinit var postViewAdapter: PostViewAdapter
 
     override fun onCreateView(inflater: LayoutInflater,
@@ -27,21 +23,15 @@ class PostListFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        subredditViewModel.subreddit.observe(viewLifecycleOwner, subredditObserver)
-
         postViewAdapter = PostViewAdapter(arrayListOf())
 
         post_list.apply {
             layoutManager = LinearLayoutManager(activity)
             adapter = postViewAdapter
         }
-
-        button.setOnClickListener{
-            subredditViewModel.onTestClicked()
-        }
     }
 
-    private val subredditObserver = Observer<Subreddit> { subreddit ->
-        postViewAdapter.update(subreddit.children)
+    fun setPosts(posts: List<Post>) {
+        postViewAdapter.update(posts)
     }
 }
