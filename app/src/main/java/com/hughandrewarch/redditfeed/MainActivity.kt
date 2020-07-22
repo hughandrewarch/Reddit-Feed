f0bed2e05b8b527c418b8dd8c03aeebcc89fb721
hughandrewarch <hughandrewarch@gmail.com>
2020-07-22 16:06:01 -0400

    link subreddit viewmodel to PostFragment

--- app/src/main/java/com/hughandrewarch/redditfeed/MainActivity.kt
+++ app/src/main/java/com/hughandrewarch/redditfeed/MainActivity.kt
@@ -1,25 +1,16 @@
 package com.hughandrewarch.redditfeed
 
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
-import com.hughandrewarch.redditfeed.databinding.ActivityMainBinding
-import com.hughandrewarch.redditfeed.domain.model.Subreddit
-import com.hughandrewarch.redditfeed.viewmodels.SubredditViewModel
 import com.hughandrewarch.redditfeed.views.fragments.PostListFragment
 
 class MainActivity : AppCompatActivity() {
 
-    private val subredditViewModel: SubredditViewModel by viewModels()
     private val postListFragment = PostListFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val activityMainBinding: ActivityMainBinding =
-            DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(R.layout.activity_main)
 
         val fragmentTransaction = supportFragmentManager.beginTransaction()
 
@@ -27,15 +18,5 @@ class MainActivity : AppCompatActivity() {
             postListFragment
         )
         fragmentTransaction.commit()
-
-        subredditViewModel.subreddit.observe(this, subredditObserver)
-        activityMainBinding.viewModel = subredditViewModel
-        activityMainBinding.executePendingBindings()
-
-        subredditViewModel.getSubreddit("kotlin")
-    }
-
-    private val subredditObserver = Observer<Subreddit> { subreddit ->
-        postListFragment.setPosts(subreddit.children)
     }
 }
--- app/src/main/java/com/hughandrewarch/redditfeed/views/fragments/PostListFragment.kt
+++ app/src/main/java/com/hughandrewarch/redditfeed/views/fragments/PostListFragment.kt
@@ -4,15 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.bumptech.glide.manager.SupportRequestManagerFragment
 import com.hughandrewarch.redditfeed.R
+import com.hughandrewarch.redditfeed.databinding.PostListFragmentBinding
 import com.hughandrewarch.redditfeed.domain.model.Post
+import com.hughandrewarch.redditfeed.domain.model.Subreddit
+import com.hughandrewarch.redditfeed.viewmodels.SubredditViewModel
 import com.hughandrewarch.redditfeed.views.adapter.PostViewAdapter
 import kotlinx.android.synthetic.main.post_list_fragment.*
 
 class PostListFragment : Fragment() {
+
+    private val subredditViewModel: SubredditViewModel by activityViewModels()
     private lateinit var postViewAdapter: PostViewAdapter
 
     override fun onCreateView(
@@ -20,7 +27,17 @@ class PostListFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.post_list_fragment, container, false)
+
+        val postListFragmentBinding: PostListFragmentBinding =
+            DataBindingUtil.inflate<PostListFragmentBinding>(
+                inflater, R.layout.post_list_fragment, container, false
+            )
+
+        subredditViewModel.subreddit.observe(viewLifecycleOwner, subredditObserver)
+        postListFragmentBinding.viewModel = subredditViewModel
+        postListFragmentBinding.executePendingBindings()
+
+        return postListFragmentBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -34,10 +51,8 @@ class PostListFragment : Fragment() {
             layoutManager = LinearLayoutManager(activity)
             adapter = postViewAdapter
         }
-    }
 
-    fun setPosts(posts: List<Post>) {
-        postViewAdapter.update(posts)
+        subredditViewModel.getSubreddit("kotlin")
     }
 
     fun launchPostFragment(post: Post) {
@@ -47,8 +62,12 @@ class PostListFragment : Fragment() {
         fragmentTransaction.replace(
             R.id.frame,
             PostFragment.newInstance(post)
-        )
+        ).addToBackStack(null)
+
         fragmentTransaction.commit()
     }
 
+    private val subredditObserver = Observer<Subreddit> { subreddit ->
+        postViewAdapter.update(subreddit.children)
+    }
 }
