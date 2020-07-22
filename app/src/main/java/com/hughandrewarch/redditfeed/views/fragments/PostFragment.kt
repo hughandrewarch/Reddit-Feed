34ef12606e95176b13d889082c89d600a2f153fd
hughandrewarch <hughandrewarch@gmail.com>
2020-07-22 11:21:37 -0400

    expand post fragment

--- app/src/main/java/com/hughandrewarch/redditfeed/views/fragments/PostFragment.kt
+++ app/src/main/java/com/hughandrewarch/redditfeed/views/fragments/PostFragment.kt
@@ -1,10 +1,16 @@
 package com.hughandrewarch.redditfeed.views.fragments
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.hughandrewarch.redditfeed.R
 import com.hughandrewarch.redditfeed.domain.model.Post
 import kotlinx.android.synthetic.main.post_fragment.*
@@ -32,6 +38,32 @@ class PostFragment: Fragment() {
     
     private fun setData() {
         title.text = post?.title
+        author.text = post?.author
+
+        Glide.with(thumbnail)
+            .load(post?.thumbnail)
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    thumbnail.visibility = View.GONE
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    return false
+                }
+            })
+            .into(thumbnail)
     }
 
     companion object {
